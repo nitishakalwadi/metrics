392b1d737f96fbca7475c9e837be74c545f6d227
Nitish Akalwadi <nitish.akalwadi@CLPT0998.local>
2021-08-09 20:32:47 +0530

    test

--- js/metrics.js
+++ js/metrics.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	$.get("/metrics/data/in/csv/2021/32/primary_api.csv", function(data) {
 		console.log(data);
 		var csv = $.csv.toObjects(data);
+		console.log(csv);
 	});
 
 });
