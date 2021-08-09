c57e8f98dc5da88db0d82cc142a9e8a7558f57d0
Nitish Akalwadi <nitish.akalwadi@CLPT0998.local>
2021-08-09 20:55:54 +0530

    test

--- js/metrics.js
+++ js/metrics.js
@@ -1,16 +1,29 @@
+var baseUrl = "/metrics/data/in/csv/";
+var baseUrltest = "/metrics/data/in/csv/2021/32/primary_api.csv";
+
+var primaryApiErrorStatsChartObj = {};
+
+
 $(document).ready(function() {
 
 	$.get("/metrics/data/in/meta.json", processAllWeeks);
 
 	
 	$.get("/metrics/data/in/csv/2021/32/primary_api.csv", function(data) {
-		console.log(data);
+		// console.log(data);
 		var csv = $.csv.toObjects(data);
-		console.log(csv);
+		// console.log(csv);
 	});
 
 });
 
 function processAllWeeks(data) {
+	for(weekData in data) {
+		var url = baseUrl + weekData.year + "/" + weekData.week + "/" + weekData.primary_api_error_stats;
+		$.get(url, processPrimaryApiErrorStatsSingleFile);
+	}
+}
+
+function processPrimaryApiErrorStatsSingleFile(data) {
 	console.log(data);
 }
